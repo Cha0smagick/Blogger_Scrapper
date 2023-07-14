19df0a59c34a61a5691c4ae6b9daaf7e0e75ef70
Cha0smagick <alekos20@gmail.com>
2023-07-14 15:47:45 -0500

    Update Blogger_scrapper_v10.py
    version 1.1 
    
    - Añadidos blog de wordpress
    - Fechas en desarrollo

--- Blogger_scrapper_v10.py
+++ Blogger_scrapper_v10.py
@@ -1,25 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Ask for the URL of the Blogger blog
-url = input("Please enter the URL of the Blogger blog: ")
+# Ask for the URL of the blog
+url = input("Please enter the URL of the blog: ")
 
 # Ask for the number of entries to review
 num_entries = int(input("Please enter the number of entries to review: "))
 
 # Print the URL of the blog
-print("The URL of the blog is: ", url)
+print("The URL of the blog is:", url)
 
 # Make a request to the website
 response = requests.get(url)
 
-# Parse the HTML or XML of the website
+# Parse the HTML of the website
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Find all the elements with the class "post-title"
-titles = soup.find_all(class_="post-title")
+# Find all the elements with the appropriate class or tag for both platforms
+if "blogspot.com" in url:
+    titles = soup.find_all(class_="post-title")
+    dates = soup.find_all(class_="datePublished")
+    links = soup.find_all(class_="post-title")
+elif "wordpress.com" in url:
+    titles = soup.find_all(class_="entry-title")
+    dates = soup.find_all("time", class_="entry-date published")
+    links = soup.find_all(class_="entry-title")
+else:
+    print("Unsupported URL format.")
+    exit()
 
-# Extract the text from each element and print it
-for title in titles[:num_entries]:
+# Extract the text, URL, and date from each entry and print them
+for i in range(min(num_entries, len(titles))):
+    title = titles[i]
+    date = dates[i].text.strip() if dates else "Unknown Date"
+    link = links[i].find('a')['href']
     print(title.text)
-    print("URL: ", title.find('a')['href'])
+    print("Publication Date:", date)
+    print("URL:", link)
+    print()
