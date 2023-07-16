e8cfb38d6593202db528c22648e6a54343077ae3
Cha0smagick <alekos20@gmail.com>
2023-07-15 21:45:54 -0500

    Update Blogger_scrapper_v10.py
    Version 1.3
    
    
    - añadido exportar a .csv para mejor organizacion del minado

--- Blogger_scrapper_v10.py
+++ Blogger_scrapper_v10.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # Ask for the URL of the blog
 url = input("Please enter the URL of the blog: ")
@@ -29,24 +30,46 @@ else:
     print("Unsupported URL format.")
     exit()
 
-# Extract and print the information from each entry
+# Create a list to store the data
+data = []
+
+# Extract and store the information from each entry
 for i in range(min(num_entries, len(titles))):
     title = titles[i]
     date = dates[i].text.strip() if i < len(dates) else "Unknown Date"
     link = links[i].find('a')['href']
 
-    print("\nTitle:", title.text, end="")  # Remove the newline after "Title:"
-    print("Publication Date:\n\n",date)
-    print("\n\nURL:\n\n",link)
+    entry_data = {
+        'Title': title.text,
+        'Publication Date': date,
+        'URL': link
+    }
 
     # Make a request to the entry URL
     entry_response = requests.get(link)
     entry_soup = BeautifulSoup(entry_response.text, "html.parser")
 
-    # Find and print the content of the entry
+    # Find and store the content of the entry
     entry_content = entry_soup.find(class_="entry-content")
     if entry_content:
-        print("\n\nContent:\n")
-        print(entry_content.text.strip())
+        entry_data['Content'] = entry_content.text.strip()
 
-    print("\n" + "=" * 50)  # Double line of dashes as a separator
+    data.append(entry_data)
+
+    print("\nTitle:", title.text, end="")
+    print("Publication Date:\n\n", date)
+    print("\n\nURL:\n\n", link)
+    if 'Content' in entry_data:
+        print("\n\nContent:\n")
+        print(entry_data['Content'])
+
+    print("\n" + "=" * 50)
+
+# Create a DataFrame from the data
+df = pd.DataFrame(data)
+
+# Export the DataFrame to Excel and CSV
+df.to_excel('blog_entries.xlsx', index=False)
+df.to_csv('blog_entries.csv', index=False)
+
+print("\nData exported to Excel (blog_entries.xlsx) and CSV (blog_entries.csv) files."
